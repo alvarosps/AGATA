5ef0d296026cace5d6ec05fdd13ae482972d9c06
João Flach <joaoflach@gmail.com>
2018-05-22 10:29:33 -0300

    feitas ultimas modificações: c minusculo, dois pontos, ingles->portugues

--- textMining/forms.py
+++ textMining/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class FormKeywords(forms.Form):
-    keyword_1 = forms.CharField(label = "Palavra-Chave 1 *", max_length = 100)
-    keyword_2 = forms.CharField(label = "Palavra-Chave 2", max_length = 100, required=False)
-    keyword_3 = forms.CharField(label = "Palavra-Chave 3", max_length = 100, required=False)
-    book = forms.FileField(label= "Livro-Texto (formato .txt) *")
+    keyword_1 = forms.CharField(label = "Palavra-chave 1: *", max_length = 100)
+    keyword_2 = forms.CharField(label = "Palavra-chave 2:", max_length = 100, required=False)
+    keyword_3 = forms.CharField(label = "Palavra-chave 3:", max_length = 100, required=False)
+    #book = forms.FileField(label= "Livro-Texto (formato .txt) *")
